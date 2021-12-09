6bb618104dd565086a912626cdd515356e630d8e
Макарова Юлия <70716673+MakarovaJulia@users.noreply.github.com>
2021-12-09 10:06:27 +0300

    Update AuthenticationFilter.java

--- src/main/java/ru/itis/servletsapp/filters/AuthenticationFilter.java
+++ src/main/java/ru/itis/servletsapp/filters/AuthenticationFilter.java
@@ -15,7 +15,6 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // преобразуем запросы к нужному виду
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
@@ -39,40 +38,26 @@ public class AuthenticationFilter implements Filter {
             return;
         }
 
-        // берем сессию у запроса
-        // берем только существующую, если сессии не было - то вернет null
         HttpSession session = request.getSession(false);
-        // флаг, аутентифицирован ли пользователь
         boolean isAuthenticated = false;
-        // существует ли сессия вообще?
         boolean sessionExists = session != null;
-        // идет ли запрос на страницу входа или регистрации?
         boolean isRequestOnAuthPage = request.getRequestURI().equals("/sign-in") ||
                 request.getRequestURI().equals("/sign-up");
 
         boolean isRequestOnProfilePage = request.getRequestURI().equals("/profile");
-
-        // если сессия есть
         if (sessionExists) {
-            // проверим, есть ли атрибует user?
             isAuthenticated = session.getAttribute("user") != null;
         }
 
         if (isAuthenticated && !isRequestOnAuthPage || !isAuthenticated && isRequestOnAuthPage){
-            // если авторизован и запрашивает не открытую страницу или если не авторизован и запрашивает открытую
-            // отдаем ему то, что он хочет
             filterChain.doFilter(request, response);
         } else if (isAuthenticated && isRequestOnAuthPage) {
-            // пользователь аутенцифицирован и запрашивает страницу входа
-            // - отдаем ему профиль
             response.sendRedirect("/profile");
 
         } else if(!isAuthenticated && isRequestOnProfilePage){
             response.sendRedirect("/sign-in");
 
         } else {
-            // если пользователь не аутенцицицирован и запрашивает другие страницы
-
             response.sendRedirect("/main");
         }
     }
